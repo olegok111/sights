072c17789899707801e74c842f92d7ad482627c2
olegok111 <olegok2003@mail.ru>
2019-02-28 22:11:55 +0300

    v. pre1
    Добавлена дизайнерская кнопка.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.config import Config
 import dbwrite
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.properties import NumericProperty
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
 '''
 Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '1080')
@@ -25,46 +24,10 @@ def add_10(instance):
     prev_points = dbwrite.pget(instance.text)['points']
     dbwrite.pwrite(instance.text, prev_points+10)
 
-'''
-class TestApp(App):
 
-    def build(self):
-        global sm
-        all_scr_lyt = FloatLayout()
-        button_exit = Button(text='X', font_size=72, background_color=[1, 0, 0, 1], background_normal='',
-                             pos=(680, 510), size_hint=(.15, .15), on_press=exit_action)
-        main_btn_lyt = BoxLayout(orientation='vertical', padding=[120], spacing=40,
-                                 pos=(0, 0), size_hint=(1, .85))
-        btn_1 = Button(text='Продолжить', on_press=console_debug_output)
-        btn_2 = Button(text='Сменить игрока', on_press=self.trans)
-        btn_3 = Button(text='nond', on_press=add_10)
-        all_scr_lyt.add_widget(button_exit)
-        main_btn_lyt.add_widget(btn_1)
-        main_btn_lyt.add_widget(btn_2)
-        main_btn_lyt.add_widget(btn_3)
-        all_scr_lyt.add_widget(main_btn_lyt)
+class ImageButton(ButtonBehavior, Image):
+    pass
 
-        self.sm = ScreenManager()
-        menu_scr = Screen(name='menu')
-        menu_scr.add_widget(all_scr_lyt)
-        self.sm.add_widget(menu_scr)
-
-        empty_scr = Screen(name='empty')
-        self.sm.add_widget(empty_scr)
-        empty_scr.add_widget(Button(text='test', on_press=self.trans))
-
-        self.screens = [menu_scr, empty_scr]
-        self.screen_number = 0
-        return self.sm
-
-    def trans(self, instance):
-        self.screen_number = (self.screen_number + 1) % len(self.screens)
-        self.sm.current = self.screens[self.screen_number].name
-
-
-if __name__ == '__main__':
-    TestApp().run()
-'''
 
 with open('structure.kv', encoding='utf8') as f:
     Builder.load_string(f.read())
@@ -89,6 +52,8 @@ sm.add_widget(QuestChooseScreen(name='quests'))
 
 
 class SightsApp(App):
+    question = NumericProperty(0)
+    im = Image(source = 'icons/back.png')
 
     def build(self):
         return sm
